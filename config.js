6b89e0e86452a6ad2fda2f75af890f02757e4bba
shogoyos-dotcom <shogoyos@gmail.com>
2026-09-10 17:31:34 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Supabase Dashboard ▸ Project Settings ▸ API bo‘limidan oling.
 // (anon/public key — bu ochiq kalit, brauzerda bo‘lishi normal.)
 window.BIRGA_CONFIG = {
-  supabaseUrl: "https://smvbzyenejnqrncfjelu.supabase.co",
+  supabaseUrl: "https://smvbzyenejnqrncfjelu.supabase.co/rest/v1/",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtdmJ6eWVuZWpucXJuY2ZqZWx1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwMzAzMTQsImV4cCI6MjEwNDYwNjMxNH0.wHtnavC9-oXDMDT0aZFlR-neQkBhe3-ORZDQ5FUXDN0"
 };
